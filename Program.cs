826fee8983f53922ee911b8a61f4ad63504abafd
mengyue99 <58315457+mengyue99@users.noreply.github.com>
2019-12-27 09:38:30 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace muji
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int x = 0; x <= 20; x++)
+            {
+                for (int y = 0; y <= 100 / 3; y++)
+                {
+                    int z = 100 - x - y;
+                    if (5 * x + 3 * y + z / 3 == 100 && z % 3 == 0)
+                    {
+                        Console.WriteLine("公鸡{0}只，母鸡{1}只，小鸡{2}只", x, y, z);
+                    }
+                }
+            }
+        }
+    }
+}
